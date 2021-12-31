432819684129117d49bd7791194566eddbdfc1c5
pmartinsb <pedro.martins+github@meliuz.com.br>
2021-12-30 21:15:40 -0300

    Create Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
-
-import { Container, Title } from "./styles";
+import { HighlightCard } from "../../components/HighlightCard";
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+} from "./styles";
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                        source={{ uri: 'https://avatars.githubusercontent.com/u/22282704?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Pedro</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>
+
+            <HighlightCard />
+
+
         </Container>
     )
 }
