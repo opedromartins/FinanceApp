2ae531046b451a8edad7a72d6d51ad0688bec569
pmartinsb <pedro.martins+github@meliuz.com.br>
2021-12-31 00:21:31 -0300

    Create Highlight Cards

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons'
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
     flex: 1;
@@ -13,13 +14,14 @@ export const Header = styled.View`
     background-color: ${({theme}) => theme.colors.primary};
 
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
     width: 100%;
     padding: 0 24px;
+    margin-top: ${getStatusBarHeight() + RFValue(28)}px;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
@@ -61,4 +63,20 @@ export const HighlightCards = styled.ScrollView.attrs({
     horizontal: true,
     showsHorizontalScrollIndicator: false,
     contentContainerStyle: { paddingHorizontal: 24 },
-})``;
+})`
+    width: 100%;
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+    flex: 1;
+    padding: 0 ${RFValue(24)}px;
+
+    margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+    font-family: ${({theme}) => theme.fonts.regular};
+    font-size: ${RFValue(18)}px;
+`;
